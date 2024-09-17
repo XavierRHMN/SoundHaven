d7fd1cb502c2002e133585ce0b74e5dfee7dfca1
Xavier <xrahman@student.ubc.ca>
2024-09-17 16:54:36 -0700

    Added playlist banner and info

---
+++ SoundHeaven/Controls/SongInfoControl.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SoundHeaven.Controls
+{
+    public partial class SongInfoControl : UserControl
+    {
+        public SongInfoControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
