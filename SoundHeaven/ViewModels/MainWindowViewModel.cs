c9a92f02e2ea19aa47c5eb9a2884f5cb6ab92bcf
Xavier <xrahman@student.ubc.ca>
2024-09-17 17:26:53 -0700

    scrolling text implemented

--- SoundHeaven/ViewModels/MainWindowViewModel.cs
+++ SoundHeaven/ViewModels/MainWindowViewModel.cs
@@ -2,21 +2,14 @@
 using Avalonia.Input;
 using Avalonia.Threading;
 using ReactiveUI;
+using SoundHeaven.Commands;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows.Input;
 using SoundHeaven.Models;
-using SoundHeaven.Commands;
-using SoundHeaven.Controls;
-using SoundHeaven.Helpers;
 using SoundHeaven.Services;
 using SoundHeaven.Stores;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reactive;
-using System.Threading.Tasks;
 
 namespace SoundHeaven.ViewModels
 {
@@ -26,10 +19,14 @@ namespace SoundHeaven.ViewModels
         private readonly AudioPlayerService _audioPlayerService;
         public AudioPlayerService AudioPlayerService => _audioPlayerService;
         private readonly SongStore _songStore;
-        private DispatcherTimer _timer;
+        private DispatcherTimer _seekTimer;
+        private DispatcherTimer _scrollTimer;
+        private double _textWidth;
+        private double _controlWidth;
         
         private ViewModelBase _currentViewModel;
-        public ViewModelBase CurrentViewModel {
+        public ViewModelBase CurrentViewModel
+        {
             get => _currentViewModel;
             set
             {
@@ -40,27 +37,25 @@ namespace SoundHeaven.ViewModels
                 }
             }
         }
-        
-        
+
         private Song _currentSong;
         public Song CurrentSong
         {
             get => _currentSong;
             set
             {
-                
                 if (_currentSong != value)
                 {
                     _currentSong = value;
-                    OnPropertyChanged();  // Notify UI about the change
+                    OnPropertyChanged();  
                     Start();
                     SeekPosition = 0;
-                    Volume = _audioPlayerService.GetCurrentVolume(); // Assuming GetCurrentVolume() returns the current volume.
+                    Volume = _audioPlayerService.GetCurrentVolume();
                     MuteCommand.RaiseCanExecuteChanged();
                 }
             }
         }
-        
+
         private bool _isPlaying = true;
         public bool IsPlaying
         {
@@ -75,7 +70,7 @@ namespace SoundHeaven.ViewModels
                 }
             }
         }
-        
+
         private double _volume;
         public double Volume
         {
@@ -84,16 +79,13 @@ namespace SoundHeaven.ViewModels
             {
                 _volume = value;
                 OnPropertyChanged();
-                
-                // Only attempt to set volume if a song is loaded
                 if (CurrentSong != null)
                 {
                     _audioPlayerService?.SetVolume((float)_volume);
                 }
-                
             }
         }
-        
+
         private bool _isMuted;
         public bool IsMuted
         {
@@ -104,15 +96,15 @@ namespace SoundHeaven.ViewModels
                 {
                     _isMuted = value;
                     OnPropertyChanged();
-            
+
                     if (_isMuted)
                     {
-                        PreviousVolume = (float)Volume; // Save the current volume before muting
-                        Volume = 0; // Set volume to 0 when muted
+                        PreviousVolume = (float)Volume;
+                        Volume = 0;
                     }
                     else
                     {
-                        Volume = PreviousVolume; // Restore the previous volume when unmuted
+                        Volume = PreviousVolume;
                     }
                 }
             }
@@ -128,7 +120,7 @@ namespace SoundHeaven.ViewModels
                 OnPropertyChanged();
             }
         }
-        
+
         private double _seekPosition;
         public double SeekPosition
         {
@@ -138,12 +130,37 @@ namespace SoundHeaven.ViewModels
                 if (_seekPosition != value)
                 {
                     _seekPosition = value;
-                    OnPropertyChanged();  // This will notify the ViewModel and the UI
+                    OnPropertyChanged();  
                 }
             }
         }
 
+        // For the scrolling text
+        private double _titleScrollPosition;
+        public double TitleScrollPosition
+        {
+            get => _titleScrollPosition;
+            set
+            {
+                _titleScrollPosition = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _artistScrollPosition;
+        public double ArtistScrollPosition
+        {
+            get => _artistScrollPosition;
+            set
+            {
+                _artistScrollPosition = value;
+                OnPropertyChanged();
+            }
+        }
         
+        public double TextWidth { get; set; } = 200; // Estimated width of the text
+        public double ControlWidth { get; set; } = 300; // Width of the canvas/border
+
         public ObservableCollection<Playlist> Playlists { get; set; }
 
         // Commands
@@ -154,42 +171,61 @@ namespace SoundHeaven.ViewModels
         public RelayCommand ShowHomeViewCommand { get; }
         public RelayCommand ShowPlaylistViewCommand { get; }
         public RelayCommand MuteCommand { get; }
-        private SeekSliderControl _seekSliderControl;
 
-        // Constructor
         public MainWindowViewModel()
         {
-            // Initialize services
             _audioPlayerService = new AudioPlayerService();
-
-            // Initialize collections
-            _songStore = new SongStore();    
+            _songStore = new SongStore();
             Playlists = new ObservableCollection<Playlist>();
 
-            // Initialize commands
             PlayCommand = new RelayCommand(Play, CanPlay);
             PauseCommand = new RelayCommand(Pause, CanPause);
             NextCommand = new RelayCommand(Next, CanNext);
             PreviousCommand = new RelayCommand(Previous, CanPrevious);
             ShowHomeViewCommand = new RelayCommand(ShowHomeView);
             ShowPlaylistViewCommand = new RelayCommand(ShowPlaylistView);
-            CurrentViewModel = new HomeViewModel(this);
             MuteCommand = new RelayCommand(ToggleMute, CanToggleMute);
 
-            // Load initial data (optional step)
+            CurrentViewModel = new HomeViewModel(this);
+
             _songStore.LoadSongs();
-            _seekSliderControl = new SeekSliderControl();
-            InitializeTimer();
+            InitializeSeekTimer();
+            InitializeScrollTimer();
         }
 
-        private void InitializeTimer() {
-            // Initialize the timer
-            _timer = new DispatcherTimer();
-            _timer.Interval = TimeSpan.FromMilliseconds(100); // Update every 500ms
-            _timer.Tick += UpdateSeekPosition;
-            _timer.Start();
+        private void InitializeSeekTimer()
+        {
+            _seekTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+            _seekTimer.Tick += UpdateSeekPosition;
+            _seekTimer.Start();
         }
-        
+
+        private void InitializeScrollTimer()
+        {
+            _scrollTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(50) };
+            _scrollTimer.Tick += (sender, e) => ScrollText();
+            _scrollTimer.Start();
+        }
+
+        private void ScrollText()
+        {
+            // Adjust the scroll positions
+            TitleScrollPosition -= 2;
+            ArtistScrollPosition -= 2;
+
+            // Reset Title Scroll Position when it goes off screen
+            if (TitleScrollPosition <= -TextWidth)
+            {
+                TitleScrollPosition = ControlWidth;
+            }
+
+            // Reset Artist Scroll Position when it goes off screen
+            if (ArtistScrollPosition <= -TextWidth)
+            {
+                ArtistScrollPosition = ControlWidth;
+            }
+        }
+
         private void UpdateSeekPosition(object sender, EventArgs e)
         {
             if (CurrentSong != null)
@@ -197,20 +233,14 @@ namespace SoundHeaven.ViewModels
                 SeekPosition += 0.1;
             }
         }
-        
-        public void ShowHomeView() {
-            CurrentViewModel = new HomeViewModel(this);
-        }
-        
-        public void ShowPlaylistView() {
-            CurrentViewModel = new PlaylistViewModel(this);
-        }
-                
+
+        public void ShowHomeView() => CurrentViewModel = new HomeViewModel(this);
+        public void ShowPlaylistView() => CurrentViewModel = new PlaylistViewModel(this);
+
         private void Start()
         {
             if (CurrentSong != null)
             {
-                // Play the song
                 _audioPlayerService.Play(CurrentSong.FilePath);
                 IsPlaying = true;
             }
@@ -220,17 +250,12 @@ namespace SoundHeaven.ViewModels
         {
             if (!IsPlaying)
             {
-                // If the song is paused, resume it
                 _audioPlayerService.Resume();
                 IsPlaying = true;
             }
         }
 
-        private bool CanPlay()
-        {
-            // Can play if the song is paused
-            return !IsPlaying;
-        }
+        private bool CanPlay() => !IsPlaying;
 
         private void Pause()
         {
@@ -241,13 +266,8 @@ namespace SoundHeaven.ViewModels
             }
         }
 
-        private bool CanPause()
-        {
-            // Can pause only if a song is playing
-            return IsPlaying;
-        }
+        private bool CanPause() => IsPlaying;
 
-        // Navigate to the next song
         private void Next()
         {
             var nextSong = _songStore.NextSong();
@@ -258,10 +278,9 @@ namespace SoundHeaven.ViewModels
                 SeekPosition = 0;
             }
         }
-        
+
         private bool CanNext() => _songStore.CanNext;
 
-        // Navigate to the previous song
         private void Previous()
         {
             var prevSong = _songStore.PreviousSong();
@@ -272,6 +291,7 @@ namespace SoundHeaven.ViewModels
                 SeekPosition = 0;
             }
         }
+
         private bool CanPrevious() => _songStore.CanPrevious;
 
         private void OnIsPlayingChanged()
@@ -279,21 +299,9 @@ namespace SoundHeaven.ViewModels
             PlayCommand.RaiseCanExecuteChanged();
             PauseCommand.RaiseCanExecuteChanged();
         }
-        
-        private void ToggleMute()
-        {
-            IsMuted = !IsMuted;
-        }
 
-        private bool CanToggleMute() {
-            return CurrentSong != null;
-        }
+        private void ToggleMute() => IsMuted = !IsMuted;
 
-        // INotifyPropertyChanged Implementation
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        private bool CanToggleMute() => CurrentSong != null;
     }
 }
